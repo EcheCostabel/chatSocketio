830406468b1127d2702438c94d11e016facc0a2f
EcheCostabel <exequielcostabel@gmail.com>
2023-06-20 17:30:14 -0300

    starting styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,8 +33,8 @@ function App() {
 
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div className='h-screen bg-zinc-800 text-white flex items-center justify-center'>
+      <form onSubmit={handleSubmit} className='bg-zinc-900 p-10'>
         <input type="text" placeholder='Write your message...' onChange={(e) => setMessage(e.target.value)}  />
 
         <button>
